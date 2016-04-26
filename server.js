c5b4a995415e5f69219754b022f684fc3bdbebca
martin byrne <marb61a@gmail.com>
2016-04-26 16:56:13 +0000

    updated server.js

--- server.js
+++ server.js
@@ -11,9 +11,22 @@ var server = app.listen(3000);
 io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
+	
+	socket.once('disconnect', function(){
+		connections.splice(connections.indexOf(socket), 1);
+		socket.disconnect();
+		console.log('Disconnected: %s sockets connected', connections.length);
+		io.emit('disconnect');
+	});
+	
+	socket.on('messageAdded', function(payload){
+		var newMessage = {
+			timeStamp: payload.timestamp,
+			text: payload.text
+		};
 
-	connections.push(socket);
-	console.log('Connected: %s sockets connected', connections.length);
+		io.emit('messageAdded', newMessage);
+	});
 });
 
 console.log('Server is running on port 3000');
