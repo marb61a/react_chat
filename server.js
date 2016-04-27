00d27f07cc2b6756659dd29e7cdb6237513e418e
martin byrne <marb61a@gmail.com>
2016-04-27 13:22:16 +0000

    updated server.js  with userjoined function

--- server.js
+++ server.js
@@ -27,6 +27,20 @@ io.sockets.on('connection', function(socket){
 
 		io.emit('messageAdded', newMessage);
 	});
+	
+	socket.on('userJoined', function(payload){
+		var newUser = {
+			id: this.id,
+			name: payload.name	
+		};
+		
+		users.push(newUser);
+		io.emit('userJoined', users);
+		console.log('User Joined: '+payload.name);
+	});
+	
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
 });
 
 console.log('Server is running on port 3000');
